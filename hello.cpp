8760cc32fdeabf57d0aa1e30a4627dbb1d8642dd
dynamocharli <officialgaurav39@gmail.com>
2022-11-21 18:10:52 +0530

    college name added

--- hello.cpp
+++ hello.cpp
@@ -5,7 +5,7 @@ int main(){
   
   // this is basic program to print hello world in cpp
 
-  cout<<"hello world i am gaurav singh rathore i am from jaipur";
+  cout<<"hello world i am gaurav singh rathore i am from jaipur and i study in anand international collee of engineering";
 
   return 0;
  
