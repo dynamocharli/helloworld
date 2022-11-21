eccc4d76760730affa65b3e381bb303753506458
dynamocharli <118731349+dynamocharli@users.noreply.github.com>
2022-11-21 17:07:12 +0530

    Update hello.cpp
    i am from udaipur

--- hello.cpp
+++ hello.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main(){
   
   // this is basic program to print hello world in cpp
-  cout<<"hello world i am gaurav singh rathore";
+  cout<<"hello world i am gaurav singh rathore i am from jaipur";
   return 0;
  
 }
